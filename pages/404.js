25b97fcb88b3add21e3fa48383227fa645155b34
Owajigbanam Ogbuluijah ✊🏾 <xigbanam@gmail.com>
2022-01-14 18:25:17 +0000

    Dynamic Routes Details

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+import Layout from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <h1 className={utilStyles.heading2Xl}>404 - Page not found</h1>
+    </Layout>
+  )
+}
